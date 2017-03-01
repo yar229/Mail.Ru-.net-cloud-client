0f915be15523f5fbe3f3a55981727c6148a6985e
YaR <yar229@yandex.ru>
2017-03-01 14:33:36 +0300

    FolderInfoRequest parameters added

--- MailRuCloudApi/Api/Requests/FolderInfoRequest.cs
+++ MailRuCloudApi/Api/Requests/FolderInfoRequest.cs
@@ -6,17 +6,21 @@ namespace MailRuCloudApi.Api.Requests
     class FolderInfoRequest : BaseRequest<FolderInfoResult>
     {
         private readonly string _path;
+        private readonly int _offset;
+        private readonly int _limit;
 
-        public FolderInfoRequest(CloudApi cloudApi, string path) : base(cloudApi)
+        public FolderInfoRequest(CloudApi cloudApi, string path, int offset = 0, int limit = int.MaxValue) : base(cloudApi)
         {
             _path = path;
+            _offset = offset;
+            _limit = limit;
         }
 
         public override string RelationalUri
         {
             get
             {
-                var uri = $"/api/v2/folder?token={CloudApi.Account.AuthToken}&home={Uri.EscapeDataString(_path)}&offset=0&limit={int.MaxValue}";
+                var uri = $"/api/v2/folder?token={CloudApi.Account.AuthToken}&home={Uri.EscapeDataString(_path)}&offset={_offset}&limit={_limit}";
                 return uri;
             }
         }

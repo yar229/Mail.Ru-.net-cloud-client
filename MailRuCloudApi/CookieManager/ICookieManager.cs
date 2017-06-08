89f35ad950ebc4cc8296524d5de381de2f59411b
YaR <yar229@yandex.ru>
2017-06-08 17:02:53 +0300

    load/save cookies : CookieManager

---
+++ MailRuCloudApi/CookieManager/ICookieManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace MailRuCloudApi.CookieManager
+{
+    public interface ICookieManager
+    {
+        CookieContainer Load(string name);
+        void Save(string name, CookieContainer container);
+    }
+}
